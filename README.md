ace80ef4052105aa6f069b7448eaf786abbb0b50
ysi <ysi@trip.com>
2021-06-22 17:23:38 +0800

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+> 这是一个用socket.io 实现的 websocket demo
+> 简单版的聊天
---
+++ index.js
@@ -0,0 +1,17 @@
+
+var app = require('express')();
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/views/index.html');
+  });
+http.listen(3000, function (params) {
+  console.log('listening on 3000 port')
+})
+io.on('connection', function (socket) {
+  console.log('a user connected')
+  socket.on('chat message', function(msg){
+    console.log('message', msg);
+    io.emit('chat message', msg);
+  });
+})
